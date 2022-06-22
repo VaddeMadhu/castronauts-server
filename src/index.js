00f90a6bea2ae3589f2b56e4ab39975adbf55e24
VaddeMadhu <madvadde@publicisgroupe.net>
2022-06-22 09:41:17 +0530

    amended new method

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 //TODO
 const { ApolloServer } = require("apollo-server");
+
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
-server.listen({ port: process.env.PORT || 4000 }).then(() => {
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
     🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at https://studio.apollographql.com/dev
+    🔉  Listening on port ${port}
+    📭  Query at ${url}
   `);
-});
+};
+
+startApolloServer(typeDefs, resolvers);
